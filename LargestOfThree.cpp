6f04cf04c575129db22d6cedd369f704952820b5
SManivong1 <SManivong1@MY.SCCCD.EDU>
2023-02-26 14:09:58 -0800

    this is a message

--- LargestOfThree.cpp
+++ LargestOfThree.cpp
@@ -2,14 +2,14 @@
 *
 * dH 2/15/23
 * demo for C++ Programming Class
-* 
+*
 * input: three ints from user
 * processing: calculate total and largest
 * output: total and largest to system console
 *
 * References:
 *    https://www.w3schools.com/cpp/cpp_user_input.asp
-*    
+*https://www.youtube.com/watch?v=T76E09hnEuo
 
 
 
@@ -21,56 +21,54 @@
 
 int main()
 {
-    std::cout << "\n Hello from Largest of Three Program\n";
+    std::cout << "Hello from Largest of Three Program";
 
     // Declare some variables
-    int num1 = 0;
-    int num2 = 0;
-    int num3 = 0;
-    int largest = 0;
+    int week1 = 9;
+    int week2 = 10;
+    int week3 = 8;
+    int largestweek = 12;
     int total = 0;
 
-    // Get three integers from the user.
-    std::cout << "\n Enter the first integer: ";
-    std::cin >> num1;
-    std::cout << "\n Enter the second integer: ";
-    std::cin >> num2;
-    std::cout << "\n Enter the third integer: ";
-    std::cin >> num3;
+    // Interger from user
+    std::cout << "Enter the first integer: ";
+    std::cin >> week1;
+    std::cout << "Enter the second interger: ";
+    std::cin >> week2;
+    std::cout << "Enter the third interger: ";
+    std::cin >> week3;
 
-    // Calculate the total 
-    total = num1 + num2 + num3;
+
+    //Calulate the total
+    total = week1 + week2 + week3;
 
     // Find the largest
-    if (num1 > num2)
+    if (week1 > week2)
     {
-        if (num1 > num3)
+        if (week1 > week3)
         {
-            largest = num1;
+            largestweek = week1;
         }
         else
         {
-            largest = num3;
+            largestweek = week3;
         }
     }
     else
     {
-        if (num2 > num3)
+        if (week2 > week3)
         {
-            largest = num2;
+            largestweek = week2;
         }
-        else 
+        else
         {
-            largest = num3;
+            largestweek = week3;
         }
     }
 
-    // Output the largest and the total of the three ints.
-    std::cout << "\n\n The largest of " << num1 << " and " << num2 << " and " << num3 << " is " << largest;
-
-    std::cout << "\n\n The total is " << total << "\n\n";
-    std::cout << "\n\n The largest is " << largest << "\n\n";
-
-}
-
+    // Output the largest of the total of the three intergers.
+    std::cout << "The largest of " << week1 << " and " << week2 << " and " << week3 << " is " << largestweek;
+    std::cout << "Total is: " << total;
+    std::cout << "Largest is:" << largestweek;
+    return 0;
 
